921410f3d5c81d90fb46c4ac8ced952325afd4aa
Wenger Binning <wengerbinning@163.com>
2021-08-25 00:26:51 +0800

    [Module] add python module.

---
+++ python/demo.py
@@ -0,0 +1,12 @@
+#!/usr/local/bin python3
+
+"""演示模块
+
+该模块用于测试Python环境是否正常，并作为Python的第一个实验程序。
+"""
+
+__author__ = "Clark Aaron"
+__version__ = "v0.0"
+
+if __name__ == '__main__':
+    print("Python环境正常，欢迎学习Python！")
