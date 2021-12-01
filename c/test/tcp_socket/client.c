bc2770d7064042453d5c074e7ec116b682cb2e0e
Wenger Binning <wengerbinning@163.com>
2021-12-01 23:11:35 +0800

    update

--- c/test/tcp_socket/client.c
+++ c/test/tcp_socket/client.c
@@ -1,10 +1,35 @@
 #include <stdio.h>
-#include <sys/socket.h> `
+#include <unistd.h>
+#include <string.h>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 int main(int argc, const char *argv[]) {
-    int ret, socketfd;
+    int ret, sockfd;
+    struct sockaddr_in server_sockaddr;
 
-    socketfd = soc
+    sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if(sockfd < 0) {
+        printf("[ERROR](%d):socket create socket object is error!\n");
+        return -1;
+    }
+
+    memset(&server_sockaddr, 0, sizeof(server_sockaddr));
+    server_sockaddr.sin_family = AF_INET;
+    server_sockaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server_sockaddr.sin_port = htons(9000);
+
+    ret = connect(sockfd, (struct sockaddr *)&server_sockaddr, sizeof(server_sockaddr));
+    if(ret < 0) {
+        printf("[ERROR](%d) connect 127.0.0.1 is error!\n", ret);
+        return -1;
+    }
+
+    printf("[DEBUG]: connect 127.0.0.1:9000 is successful!\n");
+
+    close(sockfd);
 
     return 0;
 }
--- c/test/tcp_socket/single_server.c
+++ c/test/tcp_socket/single_server.c
@@ -1,28 +1,31 @@
 #include <stdio.h>
 #include <time.h>
 #include <string.h>
+#include <unistd.h>
 
 #include <sys/socket.h>
-#include <sys/uio.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
 #define MAXLINE 1024
 
 int main(int argc, const char * argv[]) {
-    int ret=0, listen_sockfd, conn_sockfd;
+    int ret, listen_sockfd, conn_sockfd;
     char buffer[MAXLINE];
-    struct sockaddr_in server_sockaddr, client_sockaddr;
+    struct sockaddr_in server_sockaddr;
+    struct sockaddr conn_sockaddr;
     time_t ticks;
+    
     listen_sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if( listen_sockfd < 0 ) {
         printf("[ERROR](%d): server create socket was failed.\n", listen_sockfd);
         return -1;
     }
+
     memset(&server_sockaddr, 0, sizeof(server_sockaddr));
     server_sockaddr.sin_family = AF_INET;
-    server_sockaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    server_sockaddr.sin_port = htons(0);
+    server_sockaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server_sockaddr.sin_port = htons(9000);
 
     ret = bind(listen_sockfd, (struct sockaddr*)&server_sockaddr, sizeof(server_sockaddr));
     if(ret < 0) {
@@ -35,17 +38,20 @@ int main(int argc, const char * argv[]) {
         printf("[ERROR](%d): server listen sockaddr was failed.\n", ret);
         return -1;
     }
+    printf("[DEBUG]:This service's pid is %d\n",getpid());
+    printf("[INFO]: This TCP Socket service is running in %s:%d ...\n",
+           inet_ntoa(server_sockaddr.sin_addr),
+           ntohs(server_sockaddr.sin_port));
     
     while(1) {
-        conn_sockfd = accept(listen_sockfd, (struct sockaddr*)&client_sockaddr, (socklen_t*)sizeof(client_sockaddr));
+        conn_sockfd = accept(listen_sockfd, &conn_sockaddr, sizeof(conn_sockaddr));
         if(conn_sockfd < 0) {
-            printf("[ERROR](%d): accept error: code:\n", conn_sockfd);
+            printf("[ERROR](%d): accept is error!\n", conn_sockfd);
             continue;
         }
-        printf("[INFO] found a connection from %s:%d\n",
-               inet_ntop(AF_INET,&client_sockaddr.sin_addr,buffer, sizeof(buffer)),
-               ntohs(client_sockaddr.sin_port));
+        printf("[INFO] found a connection from client\n");
 
+        inet_ntop(AF_INET,&conn_sockaddr,buffer, sizeof(buffer));
         ticks =time(NULL);
         snprintf(buffer, sizeof(buffer), "%.24s\r\n", ctime(&ticks));
         write(conn_sockfd, buffer, strlen(buffer));
